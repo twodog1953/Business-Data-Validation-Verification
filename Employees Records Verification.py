03ec7eab2e91f58de9d8b1865709bf5b20f0aef4
twodog1953 <songyuchen1997@gmail.com>
2024-10-23 17:38:15 -0700

    initial commit for emp records ver

---
+++ Employees Records Verification.py
@@ -0,0 +1,12 @@
+# verifying employee records across different database and/or platforms
+import pandas as pd
+
+def init_cleaning():
+    # todo for cleaning the initially read dataframes
+    # pick desired columns, and restructure the columns if necessary
+
+    return
+
+
+if __name__ == "__main__":
+    print("Initialized! ")
